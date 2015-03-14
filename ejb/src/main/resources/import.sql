efff09c2874484d4390ba48ee648a95eaf6db1bd
enzopuntobiancato <enzo.biancato@gmail.com>
2015-03-14 15:26:56 -0300

    trying commits

--- ejb/src/main/resources/import.sql
+++ ejb/src/main/resources/import.sql
@@ -16,5 +16,5 @@
 --
 
 -- You can use this file to load seed data into the database using SQL statements
-
 insert into Materia(id, nombre, descripcion, nivel) values (1, 'Prostodoncia', 'lalalalal', 'PRIMERO');
+insert into Materia(id, nombre, descripcion, nivel) values (1, 'Endodoncia', 'lalalalal', 'PRIMERO');
