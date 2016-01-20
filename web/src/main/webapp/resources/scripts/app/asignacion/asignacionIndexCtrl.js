db33595592251cf1b9a8ab7446510c8a63c8adf8
ilopeza <ilopezarzuaga@gmail.com>
2016-01-20 18:53:59 -0300

    modulo_asignaciones_II

--- web/src/main/webapp/resources/scripts/app/asignacion/asignacionIndexCtrl.js
+++ web/src/main/webapp/resources/scripts/app/asignacion/asignacionIndexCtrl.js
@@ -3,87 +3,219 @@ var module = angular.module('asignacionModule');
 
 module.controller('AsignacionCtrl_Index', ['$scope', '$cacheFactory', 'AsignacionSrv', '$state', '$stateParams', 'MessageSrv', 'CommonsSrv',  'PaginationService', '$mdDialog',
     function ($scope, $cacheFactory, service, $state, $stateParams, message, commons, pagination, $mdDialog) {
-        $scope.result = [
-            {
-                id: '1',
-                apellido:           'Suárez',
-                nombre:             'Luis',
-                nroDocumento:       '45267892',
-                materia:            'Protodoncia',
-                trabajoPractico:    'Trabajo Practico 1',
-                img:                'http://laaficion.milenio.com/la_aficion_mundial/Suarez-mordida_de_Suarez-memes_de_Suarez-mordida_a_Chiellini_MILIMA20140624_0389_3.jpg'
-
-            },
-            {
-                id: '2',
-                apellido:           'Zárate',
-                nombre:             'Mauro',
-                nroDocumento:       '35938590',
-                materia:            'Ortodoncia',
-                trabajoPractico:    'Trabajo Practico 2',
-                img: 'http://diariomovil.com.ar/wp-content/uploads/2014/10/mauro_zarate.jpg'
-
-            },
-            {
-                id: '3',
-                apellido:           'Zanetti',
-                nombre:             'Pupi',
-                nroDocumento:       '12267892',
-                materia:            'Protodoncia',
-                trabajoPractico:    'Trabajo Practico 1',
-                img:                'http://www.cordobatimes.com/deportes/wp-content/uploads/2013/12/zanetti-mundial.jpg'
-            },
-            {
-                id: '4',
-                apellido:           'Crespo',
-                nombre:             'Hernán',
-                nroDocumento:       '45267892',
-                materia:            'Protodoncia',
-                trabajoPractico:    'Trabajo Practico 1',
-                img:                'http://www.apurogol.net/wp-content/uploads/2008/11/hernan-crespo-real-madrid.jpg'
-            },
-            {
-                id: '5',
-                apellido:           'Batistuta',
-                nombre:             'Gabriel Omar',
-                nroDocumento:       '31267892',
-                materia:            'Protodoncia',
-                trabajoPractico:    'Obstrucción',
-                img:                'http://image.tin247.com/bongdaso/100401192152-674-468.jpg'
-            },
-            {
-                id: '6',
-                apellido:           'Alonso',
-                nombre:             'Beto',
-                nroDocumento:       '32267892',
-                materia:            'Cirugía',
-                trabajoPractico:    'Extracción',
-                img:                'http://www.rivermillonarios.com.ar/media/galeria/132/4/9/2/7/n_river_plate_idolos_millonarios_y_ex_river-1357294.jpg'
-            }
-        ];
-        //pacientes
-        $scope.filter = {};
+        $scope.filterAlumno ={};
+        $scope.filterAsignaciones ={};
+        $scope.showSections = [true, false];
+        $scope.resultAlumnos = [];
+        $scope.resultAsignaciones = [];
         $scope.data = {
-        };
+            materias :[
+                {nombre: 'Prostodoncia'},{nombre: 'Ortodoncia'},{nombre: 'Cirugia'},{nombre: 'Endodoncia'}
+            ],
+            practicas : [
+                {denominacion: 'TP 1 - Diagnostico'},{denominacion: 'TP 2 - Diagnostico 2'},{denominacion: 'TP 3 - Diagnostico 3'}
+            ],
+            estados:["Pendiente", "Reservado", "Solucionado", "Cancelado", "Solucionado en CE"]
+        }
 
         $scope.reload = function(){
             $rootScope.persistedOperation = $scope.data.persistedOperation;
             $state.go($state.current, {}, {reload: true});
         };
 
+        //Consultar
+        $scope.consultarAlumno = function(){
+            $scope.resultAlumnos =[
+                {
+                    apellido: 'Barros',
+                    nombre: 'Maximiliano',telefonoFijo: '4672819',
+                    celular: '152690514',
+                    direccion: 'Río Negro 3126 (Capital Sur)',
+                    celular: '152690514',
+                    img: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/a2fa0522433737.5631e82d148c9.png'
+                },
+                {
+                    apellido: 'Biancato',
+                    nombre: 'Enzo',
+                    nroDocumento: '34257187',
+                    telefonoFijo: '4672819',
+                    celular: '152690514',
+                    direccion: 'Río Negro 3126 (Capital Sur)',
+                    img: 'http://ih1.redbubble.net/image.15211113.6626/flat,800x800,070,f.jpg'
+                },
+                {
+                    apellido: 'López Arzuaga',
+                    nombre: 'Ignacio Javier',
+                    nroDocumento: '35054738',
+                    telefonoFijo: '4672819',
+                    celular: '152690514',
+                    direccion: 'Río Negro 3126 (Capital Sur)',
+                    img: 'https://d13yacurqjgara.cloudfront.net/users/160522/screenshots/1341146/ww3.jpg'
+                },
+                {
+                    apellido: 'García',
+                    nombre: 'Mauro',
+                    nroDocumento: '32960784',
+                    telefonoFijo: '4672819',
+                    celular: '152690514',
+                    direccion: 'Río Negro 3126 (Capital Sur)',
+                    img: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/3c3ad823775971.5632898dbfbfa.png'
+                },
+                {
+                    apellido: 'Spessot',
+                    nombre: 'Alexis',
+                    nroDocumento: '38672901',
+                    telefonoFijo: '4672819',
+                    celular: '152690514',
+                    direccion: 'Río Negro 3126 (Capital Sur)',
+                    img: 'https://d13yacurqjgara.cloudfront.net/users/160522/screenshots/1149608/dribbble.jpg'
+                }
+            ]
+        };
+        $scope.consultarAsignaciones = function(){
+            $scope.resultAsignaciones = [
+                {
+                    id: '1',
+                    paciente :{
+                        apellido:           'Suárez',
+                        nombre:             'Luis',
+                        telefonoFijo: '4672819',
+                        celular: '152690514',
+                        direccion: 'Río Negro 3126 (Capital Sur)',
+                        nroDocumento:       '45267892',
+                        materia:            'Protodoncia',
+                        trabajoPractico:    'Trabajo Practico 1',
+                        img:                'http://laaficion.milenio.com/la_aficion_mundial/Suarez-mordida_de_Suarez-memes_de_Suarez-mordida_a_Chiellini_MILIMA20140624_0389_3.jpg'
+                    },
+                    materia:{
+                        nombre: "Prostodoncia"
+                    },
+                    trabajoPractico:{
+                        nombre: 'Obturación con amalgama'
+                    },
+                    fechaCreacion: '04/10/2015'
+                },
+                {
+                    id: '2',
+                    paciente :{
+                        apellido:           'Zárate',
+                        nombre:             'Mauro',
+                        telefonoFijo: '4672819',
+                        celular: '152690514',
+                        direccion: 'Río Negro 3126 (Capital Sur)',
+                        nroDocumento:       '35938590',
+                        materia:            'Ortodoncia',
+                        trabajoPractico:    'Trabajo Practico 2',
+                        img: 'http://diariomovil.com.ar/wp-content/uploads/2014/10/mauro_zarate.jpg'
+                    },
+                    materia:{
+                        nombre: "Prostodoncia"
+                    },
+                    trabajoPractico:{
+                        nombre: 'Obturación con amalgama'
+                    },
+                    fechaCreacion: '04/10/2015'
+                },
+                {
+                    id: '3',
+                    paciente :{
+                        apellido:           'Zanetti',
+                        nombre:             'Pupi',
+                        telefonoFijo: '4672819',
+                        celular: '152690514',
+                        direccion: 'Río Negro 3126 (Capital Sur)',
+                        nroDocumento:       '12267892',
+                        materia:            'Protodoncia',
+                        trabajoPractico:    'Trabajo Practico 1',
+                        img:                'http://www.cordobatimes.com/deportes/wp-content/uploads/2013/12/zanetti-mundial.jpg'
+                    },
+                    materia:{
+                        nombre: "Prostodoncia"
+                    },
+                    trabajoPractico:{
+                        nombre: 'Obturación con amalgama'
+                    },
+                    fechaCreacion: '04/10/2015'
 
+                },
+                {
+                    id: '4',
+                    paciente :{
+                        apellido:           'Crespo',
+                        nombre:             'Hernán',
+                        telefonoFijo: '4672819',
+                        celular: '152690514',
+                        direccion: 'Río Negro 3126 (Capital Sur)',
+                        nroDocumento:       '45267892',
+                        materia:            'Protodoncia',
+                        trabajoPractico:    'Trabajo Practico 1',
+                        img:                'http://www.apurogol.net/wp-content/uploads/2008/11/hernan-crespo-real-madrid.jpg'
+                    },
+                    materia:{
+                        nombre: "Prostodoncia"
+                    },
+                    trabajoPractico:{
+                        nombre: 'Obturación con amalgama'
+                    },
+                    fechaCreacion: '04/10/2015'
+                },
+                {
+                    id: '5',
+                    paciente :{
+                      apellido:           'Batistuta',
+                      nombre:             'Gabriel Omar',
+                      telefonoFijo: '4672819',
+                      celular: '152690514',
+                      direccion: 'Río Negro 3126 (Capital Sur)',
+                      nroDocumento:       '31267892',
+                      materia:            'Protodoncia',
+                      trabajoPractico:    'Obstrucción',
+                      img:                'http://image.tin247.com/bongdaso/100401192152-674-468.jpg'
+                    },
+                    materia:{
+                        nombre: "Prostodoncia"
+                    },
+                    trabajoPractico:{
+                        nombre: 'Obturación con amalgama'
+                    },
+                    fechaCreacion: '04/10/2015'
+                },
+                {
+                    id: '6',
+                    paciente :{
+                        apellido:           'Alonso',
+                        nombre:             'Beto',
+                        telefonoFijo: '4672819',
+                        celular: '152690514',
+                        direccion: 'Río Negro 3126 (Capital Sur)',
+                        nroDocumento:       '32267892',
+                        materia:            'Cirugía',
+                        trabajoPractico:    'Extracción',
+                        img:                'http://www.rivermillonarios.com.ar/media/galeria/132/4/9/2/7/n_river_plate_idolos_millonarios_y_ex_river-1357294.jpg'
+                    },
+                    materia:{
+                        nombre: "Prostodoncia"
+                    },
+                    trabajoPractico:{
+                        nombre: 'Obturación con amalgama'
+                    },
+                    fechaCreacion: '04/10/2015'
+                }
+            ];
+        };
 
-        //Consulta
-
-        $scope.consultar = function () {
-            // executeQuery();
+        $scope.keyboardOkAlumno = function(){
+            if (event.which == 13) {
+                $scope.consultarAlumno();//executeQuery();
+            }
         }
 
-        $scope.keyboardOk = function (event) {
+        $scope.keyboardOkAsignaciones = function(){
             if (event.which == 13) {
-                //executeQuery();
+                $scope.consultarAsignaciones();//executeQuery();
             }
-        };
+
+        }
 
         //Caché
         function cacheData() {
@@ -169,48 +301,16 @@ module.controller('AsignacionCtrl_Index', ['$scope', '$cacheFactory', 'Asignacio
                 });
         };
 
-        //Restaurar
-        $scope.openRestoreDialog = function (ev, asignacionId) {
-            $mdDialog.show({
-                templateUrl: 'views/asignacion/asignacionRestore.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                locals: {id: asignacionId},
-                controller: function DialogController($scope, $mdDialog) {
-                    $scope.cancelar = function () {
-                        $mdDialog.cancel();
-                    };
-                    $scope.confirmar = function () {
-                        $mdDialog.hide();
-                    };
-                }
-            })
-                .then(function () {
-                    //Success
-                    service.restore(asignacionId)
-                        .success(function (response) {
-                            message.showMessage("Se ha dado de alta.");
-                            //executeQuery($scope.paginationData.pageNumber);
-                            Console.log(response);
-                        })
-                        .error(function (error) {
-                            message.showMessage("Se ha registrado un error en la transacción.")
-                            //executeQuery($scope.paginationData.pageNumber);
-                            Console.log(error);
-                        })
-                },
-                function () {
-                    //Failure
-                    $scope.status = 'You cancelled the dialog.';
-                    Console.log(error);
-                });
-        };
+        //Seleccionar
+        $scope.seleccionarAlumno = function(){
+            $scope.showSections[0] = false;
+            $scope.showSections[1] = true;
+        }
 
         //Métodos auxiliares
         $scope.mostrarFiltros = false;
         $scope.clickIcon = 'expand_more';
-        $scope.colorIcon = ['#00B0FF', '#00B0FF', '#00B0FF', '#00B0FF', '#00B0FF', '#00B0FF'];
+        $scope.colorIcon = ['#00B0FF', '#00B0FF', '#00B0FF', '#00B0FF', '#00B0FF', '#00B0FF','#00B0FF'];
 
         $scope.cleanFilters = function () {
             $scope.filter = {};
@@ -233,17 +333,13 @@ module.controller('AsignacionCtrl_Index', ['$scope', '$cacheFactory', 'Asignacio
             $scope.colorIcon[icon] = '#00B0FF';
         };
 
-        $scope.clickIconMorph = function () {
-            if ($scope.clickIcon === 'expand_more') {
-                $scope.clickIcon = 'expand_less';
-            }
-            else {
-                $scope.clickIcon = 'expand_more';
-            }
+        $scope.cleanFiltersAlumno = function () {
+            $scope.filterAlumno = {};
+            //executeQuery();
         };
 
-        $scope.cleanFilters = function () {
-            $scope.filter = {};
+        $scope.cleanFiltersAsignaciones = function () {
+            $scope.filterAsignaciones = {};
             //executeQuery();
         };
 
